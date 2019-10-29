39e4f2e23b3b27a4bf8342d388331b790a45ff29
Kinga Polgar <kpolgar27@gmail.com>
2019-10-28 20:13:19 -0400

    Updates

---
+++ precache-manifest.7f6065fa963564b5038b8a21da4460ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3922d785dcfc20b15f39f531e359110",
+    "url": "/starwars-rolodex/index.html"
+  },
+  {
+    "revision": "d32dd3f26877667b2da7",
+    "url": "/starwars-rolodex/static/css/main.3e729e42.chunk.css"
+  },
+  {
+    "revision": "4f3e9f6c1627f0b46558",
+    "url": "/starwars-rolodex/static/js/2.4d8b3afd.chunk.js"
+  },
+  {
+    "revision": "d32dd3f26877667b2da7",
+    "url": "/starwars-rolodex/static/js/main.898b4652.chunk.js"
+  },
+  {
+    "revision": "b97bab0fca765ff36d29",
+    "url": "/starwars-rolodex/static/js/runtime-main.e915fb76.js"
+  },
+  {
+    "revision": "1583dc848e0fa38b6d89313852376ca5",
+    "url": "/starwars-rolodex/static/media/Beru Whitesun lars.1583dc84.jpg"
+  },
+  {
+    "revision": "1d303942c557bc6a3beeab56ba57a7ca",
+    "url": "/starwars-rolodex/static/media/Biggs Darklighter.1d303942.jpg"
+  },
+  {
+    "revision": "ae5458db694a90c3417fbd59db1ee79e",
+    "url": "/starwars-rolodex/static/media/C-3PO.ae5458db.jpg"
+  },
+  {
+    "revision": "237e166f57381855fa928178d4409297",
+    "url": "/starwars-rolodex/static/media/Darth Vader.237e166f.jpg"
+  },
+  {
+    "revision": "303bc390de7274a8bc55540fa9e70641",
+    "url": "/starwars-rolodex/static/media/Leia Organa.303bc390.jpg"
+  },
+  {
+    "revision": "066cc5a8fd91667d9bb5d956ec4dc6f0",
+    "url": "/starwars-rolodex/static/media/Luke Skywalker.066cc5a8.jpg"
+  },
+  {
+    "revision": "f451e481eace2e84509cc40456eda5d5",
+    "url": "/starwars-rolodex/static/media/Obi-Wan Kenobi.f451e481.jpg"
+  },
+  {
+    "revision": "fb238258b8d7e34460066c948f66b935",
+    "url": "/starwars-rolodex/static/media/Owen Lars.fb238258.jpg"
+  },
+  {
+    "revision": "c8068fb55ca465bd26cb21366bdc7ace",
+    "url": "/starwars-rolodex/static/media/R2-D2.c8068fb5.jpg"
+  },
+  {
+    "revision": "1a48d17f66e2225c130f659e68e8e368",
+    "url": "/starwars-rolodex/static/media/R5-D4.1a48d17f.jpg"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/starwars-rolodex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
